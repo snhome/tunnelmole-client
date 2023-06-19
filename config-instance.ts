1b6b3f1792ee917c8882328e0c7475a42c005c42
steven <sntong@gmail.com>
2023-06-19 23:37:27 +0800

    update

---
+++ config-instance.ts
@@ -0,0 +1,10 @@
+const instanceConfig = {
+    hostip: {
+        endpoint: "wss://expose1.sntongnas.fun"
+    },
+    runtime: {
+        enableLogging: true
+    }
+}
+
+export default instanceConfig;
